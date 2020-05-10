50e8619b061aaa6072dec9b5dc0c205fa882fd8d
yds12 <28710965+yds12@users.noreply.github.com>
2020-05-10 18:10:23 +0200

    Socket tests working.

--- test/server-test.js
+++ test/server-test.js
@@ -7,20 +7,21 @@ const server = require(path.join(__dirname, '../src/server'))
 
 const port = CONFIG.port
 
+function connectSocket(path){
+  let socketPath = 'http://localhost:' + CONFIG.port + path 
+  return socketIoClient.connect(socketPath)
+}
+
 describe('server', () => {
 
-  let httpServer
-
-  const options = {
-    host: 'localhost', port: port, path: '/', method: 'GET'
-  }
+  let httpServer, socket
 
   before((done) => {
     http.get(options, res => {
-      console.log('Server already running...')
+      console.log('is already running and...')
       done()
     }).on('error', err => {
-      console.log('Server not running. Starting it for the tests...')
+      console.log('is not running, we will start it for the tests and it...')
 
       httpServer = server.start(CONFIG)
       setTimeout((err, result) => {
@@ -33,9 +34,30 @@ describe('server', () => {
     })
   })
 
+  const options = {
+    host: 'localhost', port: port, path: '/', method: 'GET'
+  }
+
   it('should listen at ' + port, (done) => {
     http.get(options, (res) => {
       assert.equal(res.statusCode, 200)
+      done()
+    })
+  })
+
+  it('should accept socket connections', (done) => {
+    let socket = connectSocket('/index')
+    socket.on('connect', () => {
+      socket.disconnect()
+      done()
+    })
+  })
+
+  it('should respond to socket connections on tic-tac-toe with a message', 
+    (done) => {
+    let socket = connectSocket('/tictactoe')
+    socket.on('message', () => {
+      socket.disconnect()
       done()
     })
   })
