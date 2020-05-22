96d288f9f20276dd34a714c3196cd3823c2a705b
tdsimao <tdsimao@users.noreply.github.com>
2020-05-22 20:00:59 +0200

    use bootstrap css template at index.html

--- www/js/index.js
+++ www/js/index.js
@@ -12,19 +12,38 @@ socket.on('updateTables', (tables) => {
 function updateTables (tables) {
   let result = ''
   if (tables.length > 0) {
-    result += '<p>Tables:</p><ul>'
+    result += '<h1> Open Tables </h1>\n'
+    result += '<div class="row" >\n'
     for (const table of tables) {
-      result += `<li><a href="${table.game}/${table.id}">` +
-        `${table.game}:${table.id} (${getTableStatus(table)})</a></li>`
+      result += makeTableDiv(table)
     }
-    result += '</ul>'
+    '</div>\n'
   }
   ulTables.innerHTML = result
 }
 
+function makeTableDiv (table) {
+  return '\n' +
+  '<div class="col-md-4">\n' +
+  '    <div class="card mb-4 shadow-sm">\n' +
+  `        <img src="res/img/${table.game}.jpg" class="card-img-top" alt="..." width="100%" height="225">\n` +
+  '        <div class="card-body">\n' +
+  `            <p class="card-text">${table.game}:${table.id}</p>\n` +
+  '            <div class="d-flex justify-content-between align-items-center">\n' +
+  '                <div class="btn-group">\n' +
+  `                    <a type="button" class="btn btn-sm btn-outline-secondary" href="${table.game}/${table.id}">Join</a>\n` +
+  `                    <a type="button" class="btn btn-sm btn-outline-secondary" href="${table.game}/${table.id}">Watch</a>\n` +
+  '                </div>\n' +
+  `                <small class="text-muted">${getTableStatus(table)}</small>\n` +
+  '            </div>\n' +
+  '        </div>\n' +
+  '    </div>\n' +
+  '</div>'
+}
+
 function getTableStatus (table) {
   switch (table.status) {
-    case 'waitingOpponents': return 'waiting opponents...'
+    case 'waitingOpponents': return 'waiting for opponents...'
     default: return table.status
   }
 }
