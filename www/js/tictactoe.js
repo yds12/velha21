e17ec656085d8aa49464ec1db3e5f036017e7d6d
tdsimao <tdsimao@users.noreply.github.com>
2020-05-25 19:49:27 +0200

    add enterTable form to tictactoe

--- www/js/tictactoe.js
+++ www/js/tictactoe.js
@@ -144,3 +144,27 @@ btnStart.onclick = (event) => {
 btnClear.onclick = (event) => {
   socket.emit('clear')
 }
+
+const btnEnter = document.getElementById('enter')
+const enterTableId = document.getElementById('enter-table-id')
+const enterPlayerName = document.getElementById('enter-player-name')
+const gameBox = document.getElementById('game-box')
+const enterTableBox = document.getElementById('enter-table-box')
+gameBox.style.display = 'none'
+enterTableBox.style.display = 'block'
+
+btnEnter.onclick = (event) => {
+  socket.emit('enterTable', {
+    "playerName": enterPlayerName.value,
+    "tableId": enterTableId.value
+  })
+}
+socket.on('enterTableResponse', (success) => {
+  if (success){
+    gameBox.style.display = 'block'
+    enterTableBox.style.display = 'none'
+  } else {
+    gameBox.style.display = 'none'
+    enterTableBox.style.display = 'block'
+  }
+})
