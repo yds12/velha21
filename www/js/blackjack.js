1b32def7e2cd1be62972ee5f70a8a82d64c502bd
tdsimao <tdsimao@users.noreply.github.com>
2020-05-26 17:58:36 +0200

    create common.js for code shared by game clients

--- www/js/blackjack.js
+++ www/js/blackjack.js
@@ -203,37 +203,3 @@ btnClear.onclick = (event) => {
 
 // Initialization
 loadImages()
-
-
-// entering table
-
-
-const btnEnter = document.getElementById('enter')
-const enterTableId = document.getElementById('enter-table-id')
-const enterPlayerName = document.getElementById('enter-player-name')
-const gameBox = document.getElementById('game-box')
-const enterTableBox = document.getElementById('enter-table-box')
-const enterTableErrorMessage = document.getElementById('error-message')
-gameBox.style.display = 'none'
-enterTableBox.style.display = 'block'
-
-let tableId = (new URL(window.location.href)).searchParams.get('tableId')
-if (tableId) enterTableId.value = tableId
-
-btnEnter.onclick = (event) => {
-  socket.emit('enterTable', {
-    "playerName": enterPlayerName.value,
-    "tableId": enterTableId.value
-  })
-}
-
-socket.on('enterTableResponse', (response) => {
-  if (response === 'success'){
-    gameBox.style.display = 'block'
-    enterTableBox.style.display = 'none'
-  } else {
-    gameBox.style.display = 'none'
-    enterTableBox.style.display = 'block'
-    enterTableErrorMessage.innerText = response
-  }
-})
