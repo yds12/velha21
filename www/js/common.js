d5232d939f3bb8a5c2b4a24bc49dcbbbdea4c997
tdsimao <tdsimao@users.noreply.github.com>
2020-05-27 22:44:22 +0200

    include bootstrap style on enter-table-form

--- www/js/common.js
+++ www/js/common.js
@@ -1,23 +1,24 @@
 
 // entering table
-const btnEnter = document.getElementById('enter')
+const enterTableForm = document.getElementById("enter-table-form");
 const enterTableId = document.getElementById('enter-table-id')
 const enterPlayerName = document.getElementById('enter-player-name')
 const gameBox = document.getElementById('game-box')
 const enterTableBox = document.getElementById('enter-table-box')
-const enterTableErrorMessage = document.getElementById('error-message')
+const enterTableErrorMessage = document.getElementById('enter-table-error-message')
 gameBox.style.display = 'none'
 enterTableBox.style.display = 'block'
 
 let tableId = (new URL(window.location.href)).searchParams.get('tableId')
 if (tableId) enterTableId.value = tableId
 
-btnEnter.onclick = (event) => {
+enterTableForm.addEventListener('submit', (event) => {
   socket.emit('enterTable', {
     "playerName": enterPlayerName.value,
     "tableId": enterTableId.value
   })
-}
+  event.preventDefault();
+})
 
 socket.on('enterTableResponse', (response) => {
   if (response === 'success'){
