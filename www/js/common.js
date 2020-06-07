fb8a651b4409af156f5cf90f200c786911266942
tdsimao <tdsimao@users.noreply.github.com>
2020-06-07 16:14:57 +0200

    show start button after game end

--- www/js/common.js
+++ www/js/common.js
@@ -50,7 +50,7 @@ const FINISHED = 1
 socket.on('state', (state) => {
   console.log(`State ${JSON.stringify(state)} received`)
   //  gameState = state;
-  if (state.gameStatus === WAITING) {
+  if ((state.gameStatus === WAITING) || (state.gameStatus === FINISHED)) {
     btnClear.hidden = true
     btnStart.hidden = false
   } else {
